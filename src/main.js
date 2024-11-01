aebea776633e837357e968804fbce3fec439862f
tygo-payple <ty.go.payple.kr>
2024-11-01 11:42:16 +0900

    chore : Font Awesome 설치
    Font Awesome 설치 및 테스트 완료
    - 변경 :
    ✔ Font Awesome 추가 및 테스트 완료
    
    [skip CI]

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
 import "@/assets/css/fonts.css"
 import "reset-css"
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import {faInstagram,faGithub} from '@fortawesome/free-brands-svg-icons'
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
 import App from './App.vue'
 import router from './router'
 
+
 const app = createApp(App)
 
-app.use(router)
 
-app.mount('#app')
+
+library.add(faArrowDown,faInstagram,faGithub);
+
+
+
+
+app
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
+
