61da3cf072d003838d751d986ff3cdfc8d914b63
TaeyoonGo <taeyoongo007@gmail.com>
2024-11-11 23:05:11 +0900

    feat: intro-section 애니메이션 기본값 설정
    변경 : AutoAlpha 설정 및 애니메이션 기본값 적용
    
    [skip-ci]

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,50 @@
 <template>
-    <app-header/>
-        <router-view />
+    <app-header ref="header"/>
+    <main>
+        <section-intro ref="intro" id="intro"/>
+        <section-challenge id="challenge"/>
+        <SectionAbility id="ability"/>
+        <section-portfolio id="portfolio"/>
+        <section-contact id="contact"/>
+    </main>
     <app-footer/>
 </template>
 
 <script setup>
+import {gsap} from "gsap";
 import AppFooter from "@/components/layouts/AppFooter.vue";
 import AppHeader from "@/components/layouts/AppHeader.vue";
+import {onMounted, ref} from "vue";
+import SectionContact from "@/views/SectionContact.vue";
+import SectionChallenge from "@/views/SectionChallenge.vue";
+import SectionPortfolio from "@/views/SectionPortfolio.vue";
+import SectionIntro from "@/views/SectionIntro.vue";
+import SectionAbility from "@/views/SectionAbility.vue";
 
+const header = ref(null);
+
+
+function gsapSet(){
+    /*
+    * autoAlpha 처리 이후 글자 호출 그 후에 Vue.js
+    * 애니메이션 관련 호출
+    *
+    * */
+    const tl = gsap.timeline({
+        defaults:{
+            duration:0.5,
+            opacity:0
+        }})
+
+    tl.fromTo('main',{autoAlpha:0},{autoAlpha:1})
+        .from('header',{y: -100})
+        .from('.scroll-down',{y:100},"<")
+}
+
+
+onMounted(()=>{
+    gsapSet();
+})
 
 
 
